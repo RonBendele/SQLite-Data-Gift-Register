8e6c32bfe4121eeab012b79aa2841a413d923e0c
Stewart Lynch <slynch@createchsol.com>
2026-02-13 11:58:15 -0800

    Part 4 completed

--- GiftRegistry/Models/Gift.swift
+++ GiftRegistry/Models/Gift.swift
@@ -15,3 +15,13 @@
 
 
 import Foundation
+import SQLiteData
+
+@Table
+struct Gift: Identifiable {
+    let id: UUID
+    var name = ""
+    var price: Double?
+    var isPurchased = false
+    var personID: Person.ID
+}
--- GiftRegistry/Views/PersonForm.swift
+++ GiftRegistry/Views/PersonForm.swift
@@ -17,35 +17,80 @@
 import SQLiteData
 import SwiftUI
 
-struct PersonForm: View {
-    @State private var person: Person.Draft
-    @State private var name:String
-    @State private var birthDate: Date?
-    @State private var notes: String
-    init(person: Person.Draft) {
-        self._person = State(initialValue: person)
-        self._name = State(initialValue: person.name)
-        self._birthDate = State(initialValue: person.birthDate)
-        self._notes = State(initialValue: person.notes)
-    }
-    private var dateBinding: Binding<Date> {
+@MainActor
+@Observable
+class PersonFormModel {
+    var person: Person.Draft
+    var name:String
+    var birthDate: Date?
+    var notes: String
+    var dateBinding: Binding<Date> {
         Binding {
-            birthDate ?? Date.now
+            self.birthDate ?? Date.now
         } set: { setDate in
-            birthDate = setDate
+            self.birthDate = setDate
         }
     }
-    @Environment(\.dismiss) var dismiss
+    @ObservationIgnored
     @Dependency(\.defaultDatabase) var database
+    
+    @ObservationIgnored
+    @FetchAll(Gift.none) var gifts
+    
+    init(person: Person.Draft) {
+        self.person = person
+        name = person.name
+        birthDate = person.birthDate
+        notes = person.notes
+        Task {
+            await loadGifts()
+        }
+    }
+    
+    func loadGifts() async {
+        await withErrorReporting {
+            _ = try await $gifts.load(
+                Gift.all
+                    .order {
+                        ($0.isPurchased.desc(), $0.name)
+                    }
+                    .where {
+                        $0.personID == person.id
+                    },
+                animation: .default
+                )
+        }
+    }
+    
+    func addPersonButtonTapped() {
+        person.name = name
+        person.birthDate = birthDate
+        person.notes = notes
+        withErrorReporting {
+            try database.write { db in
+                try Person
+                    .upsert { person }
+                    .execute(db)
+            }
+        }
+    }
+}
+
+struct PersonForm: View {
+    @State private var model: PersonFormModel
+    init(person: Person.Draft) {
+        self._model = State(initialValue: PersonFormModel(person: person))
+    }
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        NavigationStack {
             Form {
-                TextField("Name", text: $name)
-                if birthDate != nil {
-                    HStack { 
-                        DatePicker("Birthdate", selection: dateBinding, displayedComponents: .date)
+                TextField("Name", text: $model.name)
+                if model.birthDate != nil {
+                    HStack {
+                        DatePicker("Birthdate", selection: model.dateBinding, displayedComponents: .date)
                         Button {
-                            birthDate = nil
+                            model.birthDate = nil
                         } label: {
                             Image(systemName: "xmark.circle.fill")
                         }
@@ -55,43 +100,105 @@ struct PersonForm: View {
                         Text("Birthdate")
                         Spacer()
                         Button("Add Birthdate") {
-                            birthDate = Date.now
+                            model.birthDate = Date.now
                         }
                         .buttonStyle(.borderedProminent)
                     }
                 }
-                TextField("Notes", text: $notes, axis: .vertical)
+                TextField("Notes", text: $model.notes, axis: .vertical)
+                if model.person.id != nil {
+                    Section  {
+                        List {
+                            ForEach(model.gifts) { gift in
+                                HStack {
+                                    Button {
+                                        // purchaseButtonTapped
+                                    } label: {
+                                        Image(systemName: gift.isPurchased ? "checkmark.circle.fill" : "circle")
+                                            .foregroundStyle(gift.isPurchased ? .green : .secondary)
+                                    }
+                                    .buttonStyle(.plain)
+                                    VStack(alignment: .leading) {
+                                        Text(gift.name)
+                                            .strikethrough(gift.isPurchased)
+                                        if let price = gift.price {
+                                            Text(price, format: .currency(code: "USD"))
+                                                .font(.caption)
+                                                .foregroundStyle(.secondary)
+                                        }
+                                    }
+                                    Spacer()
+                                    Button {
+                                        // EditButton Tapped
+                                    } label: {
+                                        Image(systemName: "pencil.circle")
+                                            .foregroundStyle(.blue)
+                                    }
+                                    .buttonStyle(.plain)
+                                }
+                                .swipeActions(edge: .trailing) {
+                                    Button(role: .destructive) {
+                                        // Delete Gift button Tapped
+                                    }
+                                }
+                            }
+                        }
+                    } header:  {
+                        HStack {
+                            Text("Gifts")
+                            Spacer()
+                            Button {
+                                // NewGiftButtonTapped
+                            } label: {
+                                Image(systemName: "plus.circle.fill")
+                            }
+                        }
+                    }
+                }
             }
             .navigationTitle("Person")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .confirmationAction){
                     Button(role: .confirm) {
-                        person.name = name
-                        person.birthDate = birthDate
-                        person.notes = notes
-                        withErrorReporting {
-                            try database.write { db in
-                                try Person
-                                    .upsert { person }
-                                    .execute(db)
-                            }
-                        }
+                        model.addPersonButtonTapped()
                         dismiss()
                     }
                 }
-                ToolbarItem(placement: .cancellationAction) {
-                    Button(role: .close) {
-                        dismiss()
+                if model.person.id == nil {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button(role: .close) {
+                            dismiss()
+                        }
                     }
                 }
             }
-        }
     }
 }
 
 struct PersonFormPreview: PreviewProvider {
     static var previews: some View {
-        PersonForm(person: Person.Draft())
+        NavigationStack {
+            PersonForm(person: Person.Draft())
+                .previewDisplayName("New Person")
+        }
+    }
+}
+
+#Preview {
+    let person = prepareDependencies {
+         do {
+             try $0.bootstrapDatabase()
+             try $0.seedDatabaseForPreviews()
+             return try $0.defaultDatabase.read { db in
+                 try Person.find(UUID(0))
+                     .fetchOne(db)!
+             }
+         } catch {
+             fatalError("Failed to bootstrap database for previews: \(error)")
+         }
+     }
+    NavigationStack {
+        PersonForm(person: Person.Draft(person))
     }
 }
